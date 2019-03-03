b84dc6ba86fe29783102e3ca16e8a061d2a570d3
WinnyPuh <coolja@mail.ru>
2019-03-03 18:26:33 +0300

    1.4. JUnit[#116934]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,21 +1,28 @@
-/**
- * Package for calculate task.
- *
- * @author Jora Abjora
- * @version 1
- * @since 0.1
- */
 package ru.job4j.calculate;
 
 /**
  * Объект класса {@code Calculate} выводит на экран "Hello world!"
+ *
+ * @author Jora Abjora
+ * @version $Id$
+ * @since 0.1
  */
 public class Calculate{
   /**
-   * main
+   * Method main
    * @param args - args.
    */
   public static void main(String[] args) {
     System.out.println("Hello world!");
+  }
+
+  /**
+   * Method echo
+   * @param name Your name
+   * @return Echo plus your name
+   */
+
+  public String echo(String name) {
+    return "Echo, echo, echo : " + name;
   }
 }
---
+++ chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.calculate;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test
+ *
+ * @author Jora Abjora
+ * @version $Id$
+ * @since 0.1
+ */
+
+public class CalculateTest {
+    /**
+     * Test echo
+     */
+    @Test
+    public void whenTakeNmaeThreeEchoPlusName(){
+        String input = "Jora Abjora";
+        String expect = "Echo, echo, echo : Jora Abjora";
+        Calculate calc = new Calculate();
+        String result = calc.echo(input);
+        assertThat(result, is(expect));
+    }
+}
