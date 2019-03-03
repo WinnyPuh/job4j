1a72957a06e694992f8db4240bc827b59c7d73f3
WinnyPuh <coolja@mail.ru>
2019-03-03 15:41:31 +0300

    1.3. JavaDoc. Code convention [#116933]

---
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -0,0 +1,21 @@
+/**
+ * Package for calculate task.
+ *
+ * @author Jora Abjora
+ * @version $Id$
+ * @since 0.1
+ */
+package ru.job4j.calculate;
+
+/**
+ * Объект класса {@code Calculate} выводит на экран "Hello world!"
+ */
+public class Calculate{
+  /**
+   * main
+   * @param args - args.
+   */
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+}
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package for calculate task.
+ *
+ * @author Jora Abjora
+ * @version $Id$
+ * @since 0.1
+ */
+package ru.job4j.calculate;
