6923dc53cd17ba2ffc6c4dfdde2ae97f524ec686
WinnyPuh <coolja@mail.ru>
2019-03-08 14:45:30 +0300

    5.4. Построить пирамиду в псевдографике.[#116907]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Paint.java
@@ -0,0 +1,41 @@
+package ru.job4j.loop;
+
+/**
+ * 5.4. Построить пирамиду в псевдографике.[#116907].
+ * @author Jora Abjora.
+ * @version $Id$.
+ * @since 0.1
+ */
+public class Paint {
+    /**
+     * Метод рисует пирамиду из символа ^ и пробелов.
+     * @param height - высота.
+     * @return - пирамида.
+     */
+    public String piramid(int height) {
+        StringBuilder screen = new StringBuilder();
+        int width = height;
+        //return screen.toString();
+
+        for (int row = 0; row != height; row++) {
+            for (int column = width-1; column != 0; column--) {
+                if (row >= column) {
+                    screen.append("^");
+                } else {
+                    screen.append(" ");
+                }
+            }
+            for (int column = 0; column != width; column++) {
+                if (row >= column) {
+                    screen.append("^");
+                } else {
+                    screen.append(" ");
+                }
+            }
+            screen.append(System.lineSeparator());
+        }
+
+
+        return screen.toString();
+    }
+}
