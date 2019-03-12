ddf15bcfb8bf5816087df4b3cc01fe11bfa49a6f
WinnyPuh <coolja@mail.ru>
2019-03-12 23:21:24 +0300

    6.4. Слово начинается с ...[#116913]

---
+++ chapter_001/src/test/java/ru/job4j/array/ArrayCharTest.java
@@ -0,0 +1,29 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+/**
+ * Test.
+ * @author Jora Abjora
+ * @version $Id$
+ * @since 0.1
+ */
+public class ArrayCharTest {
+
+    @Test
+    public void whenStartWithPrefixThenTrue() {
+        ArrayChar word = new ArrayChar("Hello");
+        boolean result = word.startWith("He");
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenStartWithPrefixThenFalse() {
+        ArrayChar word = new ArrayChar("Hello");
+        boolean result = word.startWith("Hi");
+        assertThat(result, is(false));
+    }
+}
