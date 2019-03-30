88f2914be1e26745580cdb2651c64c82853347df
WinnyPuh <coolja@mail.ru>
2019-03-30 22:31:23 +0300

    1. Реализация профессий в коде[#116991]

---
+++ chapter_002/src/main/java/ru/job4j/professions/Diagnose.java
@@ -0,0 +1,9 @@
+package ru.job4j.professions;
+
+public class Diagnose {
+    private String diagnose;
+
+    public void setDiagnose(String diagnose) {
+        this.diagnose = diagnose;
+    }
+}
---
+++ chapter_002/src/main/java/ru/job4j/professions/Profession.java
@@ -0,0 +1,9 @@
+package ru.job4j.professions;
+
+public class Profession {
+    private String name;
+
+    public String getName() {
+        return this.name;
+    }
+}
